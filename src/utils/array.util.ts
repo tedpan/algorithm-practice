84b0871cff48bddb3b8ba02e16092a248a8a854e
tedpan <panpengdi@outlook.com>
2021-04-06 00:19:12 +0800

    feat: 数组工具

---
+++ src/utils/array.util.ts
@@ -0,0 +1,25 @@
+/** 生成一个从1开始递增 长度为length的数组 */
+export function generateArray(length: number): number[] {
+  return Array(length)
+    .fill(1)
+    .map((item, index) => item + index);
+}
+
+/** 洗牌函数，打乱数组 */
+export function shuffle<T>(arr: T[]): T[] {
+  for (let index = 0; index < arr.length; index++) {
+    // 每一项 跟 余下数组范围 [index+1,arr.length-1] 中随机一项交换位置
+    const randomIndex = Math.floor(Math.random() * (arr.length - index - 2) + index + 1);
+    swap(arr, index, randomIndex);
+  }
+  return arr;
+}
+
+/** 交换数组两项的位置 */
+export function swap(arr: any[], index1: number, index2: number) {
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
+
+export function generateRandomArray(length: number) {
+  return shuffle(generateArray(length));
+}
