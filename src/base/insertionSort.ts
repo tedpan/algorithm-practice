254bb40ac60a8a79114c88b6d88a82ac75c6317b
tedpan <panpengdi@outlook.com>
2021-05-04 22:32:01 +0800

    upd: 插入排序

---
+++ src/base/insertionSort.ts
@@ -0,0 +1,33 @@
+import { generateRandomArray } from '../utils/array.util';
+
+/**
+ * 插入排序
+ * 逐项按排序规则插入
+ * 时间复杂度 O(n2)
+ */
+export default function insertionSort<T>(arr: T[]): T[] {
+  const len = arr.length;
+  // 从第二项逐项进行插入
+  for (let index = 1; index < len; index++) {
+    const valueTemp = arr[index];
+    let indexTemp = index;
+    while (indexTemp !== 0 && valueTemp < arr[indexTemp - 1]) {
+      // 向后移动
+      arr[indexTemp] = arr[indexTemp - 1];
+      indexTemp--;
+    }
+    if (indexTemp !== index) {
+      arr[indexTemp] = valueTemp;
+    }
+  }
+  return arr;
+}
+
+function test() {
+  const ramdomArr = generateRandomArray(10);
+  console.log('randamArr:', ramdomArr);
+  const sortArr = insertionSort(ramdomArr);
+  console.log('sortArr:', sortArr);
+}
+
+test();
