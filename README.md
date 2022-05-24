79cb0f10454fc7513a898484d2c72e08cffa18e0
tedpan <panpengdi@outlook.com>
2022-05-24 12:48:16 +0800

    upd: 调整commitlint rules和readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # algorithm-practice
 
 算法练习
+
+## 单元测试
+
+```text
+  npm test 文件名（可以不写全，自动搜索test文件夹下的test文件。）
+
+```
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ['ali'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'perf',
+        'feat',
+        'fix',
+        'refactor',
+        'docs',
+        'chore',
+        'style',
+        'revert',
+        'test',
+        'upd',
+      ],
+    ],
+  },
 };
